bb91ab1ee676fbc5beb13e3e6a2223e7461d7b7b
BrandonSev <seveste.brandon@gmail.com>
2022-06-23 11:15:18 +0200

    feat: page realisations created with modal + favicon

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ categories }) {
       <section id="hero">
         <Hero
           text={
-            "Passionné de développement, je me suis formé sur différents types de languages de manière autodidacte, j'ai décidé de rejoindre une formation à la Wild Code School en septembre 2021 en tant que développeur web et mobile afin de faire de ma passion, mon métier."
+            "Passionné de développement, depuis maintenant 3 ans, je me suis formé sur différents types de langages de manière autodidacte. J'ai décidé de rejoindre une formation à la Wild Code School en septembre 2021 en tant que développeur web et web mobile afin de faire de ma passion, mon métier."
           }
           linkText={"Voir mes réalisations"}
           linkUrl={"/mes-realisations"}
